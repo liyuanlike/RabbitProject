26c3de3d8e9df65c64d12e59229c0dfd3ac00e51
LX1993728 <2652790899@qq.com>
2018-08-22 21:39:07 +0800

    删除批量更新项目 重新构建

--- batch-update/middle-datasource/src/main/java/com/liuxun/datasource/service/ProcessInstructService.java
+++
@@ -1,8 +0,0 @@
-package com.liuxun.datasource.service;
-
-
-import com.liuxun.datasource.core.domain.InstructResult;
-
-public interface ProcessInstructService {
-    public InstructResult processInstruction(String operationID,String instructRequestId);
-}
