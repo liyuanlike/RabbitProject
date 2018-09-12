8076d6e31f5319e7d91d69e06536c18f6e5dbdb9
LX1993728 <2652790899@qq.com>
2018-09-12 10:21:19 +0800

    +++++ 删除冗余项目 +++++

--- communication-of-system/api-starter-system/global-component/src/main/java/com/global/component/domain/GridManEventMessage.java
+++
@@ -1,7 +0,0 @@
-package com.global.component.domain;
-
-/**
- * @apiNote 网格员系统发送给接报系统的事件信息
- */
-public class GridManEventMessage extends EventMessage{
-}
--- communication-of-system/api-starter-system/global-component/src/main/java/com/global/component/domain/PrePositionEventMessage.java
+++
@@ -1,8 +0,0 @@
-package com.global.component.domain;
-
-/**
- * @apiNote 部委前置系统向 接报系统发送的事件消息
- * @author liuxun
- */
-public class PrePositionEventMessage extends EventMessage {
-}
